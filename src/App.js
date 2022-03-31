5a2da52d469271c079782dcd3694ba17b81a9a4d
Paul DABIN <dabin.paul@gmail.com>
2022-03-31 21:24:42 +0200

    What is a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //state (données)
+  const [title, setTitle] = useState("Paul");
+  const [fruit, setFruit] = useState("fraise");
+  const [number, setNumber] = useState(0);
+
+  //comportements
+  const handleClick = ()=>{
+    //alert("Je suis cliqué")
+    console.log(number)
+    //number = number + 1
+    setNumber(number+1) 
+    console.log(number)
+  }
+
+  //affichage
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Paul
-        </a>
-      </header>
+    <div>
+      <div>{number}</div>
+      <button onClick={handleClick}>Incrémente</button>
     </div>
   );
 }
