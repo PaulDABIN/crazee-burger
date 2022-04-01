75d8210058b2baf0bec6203778d43506130dd2c2
Paul DABIN <dabin.paul@gmail.com>
2022-04-01 19:55:46 +0200

    App.js > Clean Up

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import { useState } from "react";
 
 function App() {
   //state (données)
-  const [title, setTitle] = useState("Paul");
-  const [fruit, setFruit] = useState("fraise");
-  const [number, setNumber] = useState(0);
+  const title = "Bienvenue chez nous";
 
   //comportements
-  const handleClick = ()=>{
-    //alert("Je suis cliqué")
-    console.log(number)
-    //number = number + 1
-    setNumber(number+1) 
-    console.log(number)
-  }
+  
 
   //affichage
   return (
-    <div>
-      <div>{number}</div>
-      <button onClick={handleClick}>Incrémente</button>
-    </div>
+    <div>{title}</div>
   );
 }
 
