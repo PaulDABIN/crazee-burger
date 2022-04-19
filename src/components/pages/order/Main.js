37074f9f78c4a656dc95b6c444ae54e2279aefb2
PaulDABIN <dabin.paul@gmail.com>
2022-04-19 20:01:16 +0200

    OrderPage > Add styling

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -1,10 +1,44 @@
 import React from "react"
 import styled from "styled-components"
+import { theme } from "../../../theme"
 
 export default function Main() {
-  return <MainStyled>Main</MainStyled>
+  return <MainStyled></MainStyled>
 }
 
 const MainStyled = styled.div`
   border: 1px solid green;
+  /* background: pink; */
+  flex: 1;
+  display: grid;
+  grid-template-columns: 25% 1fr;
+  position: relative;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  .sidebar {
+    /* background: green; */
+    box-shadow: 0 0px 4px 0 rgb(0 0 0 / 20%) inset;
+  }
+  .menu-and-panel-container {
+    position: relative;
+    height: calc(95vh - 10vh);
+    display: flex;
+    box-shadow: 0 0 8px 0 rgb(0 0 0 / 20%) inset;
+
+    // menu
+    > div:first-child {
+      /* border: 3px solid red; */
+    }
+
+    .panel-appear {
+      opacity: 0.1;
+      transform: translateY(100%);
+      &.panel-appear-active {
+        opacity: 1;
+        transform: translateY(0);
+        transition: all 500ms;
+      }
+    }
+  }
 `
--- src/components/pages/order/NavBar.js
+++ src/components/pages/order/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { theme } from "../../../theme/index"
 
 export default function NavBar() {
   return <NavBarStyled>NavBar</NavBarStyled>
@@ -7,4 +8,19 @@ export default function NavBar() {
 
 const NavBarStyled = styled.div`
   border: 1px solid blue;
+  color: ${theme.colors.black};
+  background-color: ${theme.colors.white};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10vh;
+  padding: 0px 20px;
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-bottom: 1px solid ${theme.colors.greyLight};
+
+  .icon {
+    width: 30px;
+    height: 30px;
+  }
 `
